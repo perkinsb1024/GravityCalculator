44628e39257cce77345019fc781ebf3aede7e39d
Ben Perkins <bperki@umich.edu>
2014-05-04 19:41:19 -0700

    Initial Commit

---
+++ GravityCalculator/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GravityCalculator
+//
+//  Created by Ben Perkins on 5/4/14.
+//  Copyright (c) 2014 perkinsb1024. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
